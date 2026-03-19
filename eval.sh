559b927fda6b99167577ccfabbb79f7ef870a2c0
hieunguyen7337 <59214551+hieunguyen7337@users.noreply.github.com>
2026-03-19 12:35:12 +1000

    Consolidate RE package into lowercase re for eval

--- eval.sh
+++ eval.sh
@@ -1,11 +1,12 @@
 echo '========'
 echo 'Run eval'
 echo '========'
-python -u -m RE.run \
+cd re
+python -u run.py \
   --task_name "DDI" \
-  --data_dir "data/DDI/" \
-  --model_name_or_path "model" \
-  --output_dir "evaluation_result/DDI" \
+  --data_dir "../data/DDI/" \
+  --model_name_or_path "../model" \
+  --output_dir "../evaluation_result/DDI" \
   --do_eval --do_predict \
   --max_seq_length 256 \
   --per_device_eval_batch_size 4 \
